db2b4155046968cd0d7dca19b8fadaab09e99064
FedoRScorpioN <zapevalov@yandex.ru>
2024-01-14 22:56:55 +0500

    Обновление телефонного справочника из Excel-файла

--- Main.py
+++ Main.py
@@ -1,25 +1,31 @@
 import logging
 import os
+
 import scapy.all as sc
-import DB as db
+from openpyxl import load_workbook
+
+import DB
 from DB import create_tables
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 SUBNET_1 = os.getenv("SUBNET_1")
 SUBNET_2 = os.getenv("SUBNET_2")
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 
 def clear_mac_ip_pairs_table():
     try:
-        if db.conn is None or db.cursor is None:
+        if DB.conn is None or DB.cursor is None:
             logging.error('Отсутствует соединение с базой данных или указатель.')
             return
 
-        db.cursor.execute("DELETE FROM mac_ip_pairs;")
-        db.conn.commit()
+        DB.cursor.execute("DELETE FROM mac_ip_pairs;")
+        DB.conn.commit()
 
         logging.info("Таблица mac_ip_pairs успешно очищена.")
     except Exception as e:
@@ -40,35 +46,44 @@ def get_ip_mac_network(subnet):
 
 def ip_mac_to_database(mac_ip_list):
     try:
-        if db.conn is None or db.cursor is None:
+        if DB.conn is None or DB.cursor is None:
             logging.error('Отсутствует соединение с базой данных или указатель.')
             return
 
         for client in mac_ip_list:
             mac_address_upper = client['mac'].upper()
 
-            db.cursor.execute('''
+            DB.cursor.execute('''
                 INSERT INTO mac_ip_pairs (mac_address, ip_address)
                 VALUES (%s, %s)
             ''', (mac_address_upper, client['ip']))
 
-        db.conn.commit()
+        DB.conn.commit()
         logging.info('Данные добавлены в базу данных.')
     except Exception as e:
         logging.error(f'Ошибка при добавлении данных в базу данных: {str(e)}')
 
 
-def scan_and_print_to_database(subnet):
+def scan_and_to_database(subnet):
     ip_mac_network = get_ip_mac_network(subnet)
     ip_mac_network.sort(key=lambda x: int(x['ip'].split('.')[3]))
     ip_mac_to_database(ip_mac_network)
+
+
+def convert_excel_to_json(excel_file_path):
+
+def update_html_with_json(json_file_path, html_file_path):
+
+def get_column_names():
+
 
 
 if __name__ == "__main__":
     create_tables()
     clear_mac_ip_pairs_table()
 
-    scan_and_print_to_database(SUBNET_1)
-    scan_and_print_to_database(SUBNET_2)
+    scan_and_to_database(SUBNET_1)
+    scan_and_to_database(SUBNET_2)
+
 
     logging.info('Программа успешно выполнена.')
